acb9815e6d00156bd4cb08bf5cbc641b232c8908
Moritz07 <88932305+Moritz07@users.noreply.github.com>
2021-08-17 20:06:49 +0200

    added new explanation

--- mycode.js
+++ mycode.js
@@ -1,3 +1,4 @@
 "hello"
 "hallo von mir"
 alert("free vbucks: https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+"this line was added through the we Visual studio code version. Try pressing '.' whe browsing a repository"
